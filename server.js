342bf2a8fc5e7138ddd73c612c970d144403348a
Nicko-rgb <mancillanixon7@gmail.com>
2024-08-22 12:35:16 -0500

    BackCine9

--- server.js
+++ server.js
@@ -37,7 +37,7 @@ app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, 'build', 'index.html'));
-  });
+});
 
 // Ruta para registrar un nuevo usuario
 app.post('/api/registro', (req, res) => {
